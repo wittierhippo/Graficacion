61bbe09704e5dfdf182af082a61d048b3ddd62e8
Gerardo <Gerardo@LAPTOP-CS1PTL5L>
2019-04-13 00:07:20 -0500

    Añadiendo el uso de lineas

--- Graficacion/src/graficacion/Linea.java
+++ Graficacion/src/graficacion/Linea.java
@@ -1,14 +1,24 @@
+
+import java.awt.Color;
+import java.awt.Point;
+import java.awt.geom.Point2D;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package graficacion;
 
 /**
  *
  * @author Gerardo
  */
-public class Linea {
+public class Linea extends Figuras {
+    Point2D punto1;
+    Point2D punto2;
     
+        Linea(Point _p1, Point _p2, Color _color) {
+        punto1 = _p1;
+        punto2 = _p2;
+        color  = _color;
+    }  
 }
